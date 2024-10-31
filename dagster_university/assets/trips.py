32fb7b974f4e4dad1479dc5b34262c5341ff0b00
olivia-s-a <otrabalhoeschool@gmail.com>
2024-10-31 10:29:11 -0300

    Lesson 4.4.: Loading data into a database

--- dagster_university/assets/trips.py
+++ dagster_university/assets/trips.py
@@ -1,6 +1,9 @@
 import requests
 from . import constants
 from dagster import asset
+import duckdb
+import os
+
 
 
 @asset
@@ -32,3 +35,31 @@ def taxi_zone_file_bacalhau() ->  None:
     with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
         output_file.write(raw_zones.content)
 # tá certinho, diva, parabéns <3
+
+@asset(
+    deps=["taxi_trips_file_abacate"]
+)
+def taxi_trips_canjiquinha() -> None:
+    """
+      The raw taxi trips dataset, loaded into a DuckDB database
+    """
+    sql_query = """
+        create or replace table trips as (
+          select
+            VendorID as vendor_id,
+            PULocationID as pickup_zone_id,
+            DOLocationID as dropoff_zone_id,
+            RatecodeID as rate_code_id,
+            payment_type as payment_type,
+            tpep_dropoff_datetime as dropoff_datetime,
+            tpep_pickup_datetime as pickup_datetime,
+            trip_distance as trip_distance,
+            passenger_count as passenger_count,
+            total_amount as total_amount
+          from 'data/raw/taxi_trips_2023-03.parquet'
+        );
+    """
+
+    conn_dobradinha = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
+    conn_dobradinha.execute(sql_query)
+
